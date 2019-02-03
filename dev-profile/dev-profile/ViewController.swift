a1f44d356177ee6cb040a3e46a7098db8a2527a8
MalikHassnain <hassnainmalik001@gmail.com>
2019-02-03 18:36:31 +0500

    Initialize project

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by MalikHassnain on 03/02/2019.
+//  Copyright © 2019 MalikHassnain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
